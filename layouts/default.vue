e97c5018c4b9d4d4bb899bd7c8742fa21ddeaa35
tioprananda <prananda.tio@gmail.com>
2022-12-26 16:53:23 +0700

    update nuxtjs

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="wrapper">
+    <Sidebar/>
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Main Content -->
+        <div id="content">
+          <Header/>
+            <nuxt/>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/layouts/Header.vue'
+import Sidebar from '@/components/layouts/Sidebar.vue'
+export default {
+components : {
+  Header,
+  Sidebar
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxtlearn',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script : [
+      {src: 'vendor/jquery/jquery.min.js'},
+      {src: 'vendor/bootstrap/js/bootstrap.bundle.min.js'},
+      {src: 'vendor/jquery-easing/jquery.easing.min.js'},
+      {src: 'js/sb-admin-2.min.js'},
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/vendor/fontawesome-free/css/all.min.css',
+    // '@/assets/https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
+    '@/assets/css/sb-admin-2.min.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
